7e9e7edfef185097dc6eaabc2ab044ca54db6c13
kukuma <icewest@gmail.com>
2018-09-02 16:09:05 -0500

    updated to hand accumulator file

--- testcsv.py
+++ testcsv.py
@@ -1,10 +1,16 @@
 #--------------------------------------------
-#version:		1.0.0.1
+#version:		1.0
 #author:		West
-#Description:	used to prepare annual consultant benefit credit
-#Assumptions:	report end date is 03/31 each year
-#				New Business is past 12 trailing months sales credits/new business 
-# 
+#Description:	used to prepare IIROC Income Report
+#				Report run by each cycle.
+#				Data source are cycle income, RO advance, Garnishee balance and Net pay
+#				Need all active IIROC cslts and cslts who terminated within 30 days; information can be
+#				obtained from DSDB, Branuser table
+#Workflow:		read all data files based on the cycle date requested,
+#				get all cslts information from DSDB based on the cycle date requestedfilter
+#				combine all the data and filter out inactive cslts Data
+#				output to excel
+#
 #--------------------------------------------
 
 import os, re, sys, csv, time, xlrd, pyodbc, datetime
@@ -16,34 +22,47 @@ import itertools as it
 from openpyxl import load_workbook
 from shutil import copyfile
 
-sys.path.append('C:\\pycode\\libs')
+sys.path.append("C:\\pycode\\libs")
 import igtools as ig
 import dbquery as dbq
 
-#------ program starting point --------	
+#------ program starting point --------
 if __name__=="__main__":
 	## dd/mm/yyyy format
-	print 'This is the process for IIROC Income Reporting'
-	print 'Process date is ' + str(time.strftime("%m/%d/%Y"))
-	print 'Please enter the cycle end date (mm/dd/yyyy) you want to process:'
+	print("This is the process for IIROC Income Reporting")
+	print("Process date is " + str(time.strftime("%m/%d/%Y")))
+	#print("Please enter the cycle end date (mm/dd/yyyy) you want to process:")
 	#-----------------------------------------------------
 	#------- get cycle date ----------------------
-	enddate = datetime.datetime.strptime(raw_input(), '%m/%d/%Y')
+	#enddate = datetime.datetime.strptime(raw_input(), "%m/%d/%Y")
+	enddate = datetime.datetime.strptime(input("Please enter the cycle end date (mm/dd/yyyy) you want to process:"), "%m/%d/%Y")
 	startdate = ig.getCStartDate(enddate)
-	
-	print 'Cycle start date is ' + str(startdate)
-	print 'Cycle end date is ' + str(enddate)
-	
+
+	print("Cycle start date is " + str(startdate))
+	print("Cycle end date is " + str(enddate))
+
 	#--------- Handle Garnishee -------------
-	garnisheefile = "F:\\Files For\\Hai Yen Nguyen\\IIROC reporting\\Download\\Garnishee " + enddate.strftime('%m%d%Y') + ".csv"
-	print garnisheefile
-	
+	garnisheefile = "C:\\pycode\\IIROCIncome\\Garnishee " + enddate.strftime("%m%d%Y") + ".csv"
+	print(garnisheefile)
+
 	if os.path.isfile(garnisheefile):
-		file = csv.reader(open(garnisheefile, 'rb'), delimiter=',')
+		total = float(0)
+		file = csv.reader(open(garnisheefile, newline=""), delimiter=",")
 		for line in file:
-			print line
-#			if re.search(r'Report Totals', line):
-#				summary = line.split(',')
-#				print summary
-#
-	
+			if len(line) > 0:
+				if "Report Totals" in line[0]:
+					total = ig.str2float(line[3])
+					break
+		dfgarnishee = pd.read_csv(garnisheefile, engine="python", skiprows=6, skipfooter=2)
+		dfgarnishee[["Cslt No.","CACT"]].astype("int64")
+		dfgarnishee["Total Amount"] = dfgarnishee["Total Amount"].apply(ig.str2float)
+		dfgarnishee["Cycle End Date"] = dfgarnishee["Cycle End Date"].astype("datetime64")
+		if total != dfgarnishee["Total Amount"].sum().round(2):
+			print(total, " <> ", dfgarnishee["Total Amount"].sum().round(2))
+			print("please check your file")
+			sys.exit()
+		else:
+			print(total, " = ", dfgarnishee["Total Amount"].sum().round(2))
+			dfsumgarnishee = dfgarnishee.groupby(["Cslt No."])["Total Amount"].sum().round(2)
+	else:
+		print("It seems the file you need is not saved yet, please save the file first.")
